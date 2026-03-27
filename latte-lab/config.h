8fb0f0728660e19c80d6e81c17412abc29b01308
Ethan Vletter <163163171+EthanVletter@users.noreply.github.com>
2026-03-27 11:02:54 +0200

    Updated config file

--- latte-lab/config.h
+++ latte-lab/config.h
@@ -4,6 +4,7 @@
 
   const char* ssid = "YOUR_WIFI_SSID";
   const char* password = "YOUR_WIFI_PASSWORD";
-  const char* mqtt_server = "YOUR_MQTT_SERVER_IP.local";
+  const char* mqtt_server1 = "YOUR_MQTT_SERVER_IP.local";
+  const char* mqtt_server2 = "YOUR_MQTT_SERVER_IP.local";
   
 #endif
