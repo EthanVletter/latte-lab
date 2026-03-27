86ec739c6abb3e2be7958d9c7a82dc1ba253fd07
Ethan Vletter <163163171+EthanVletter@users.noreply.github.com>
2026-03-27 11:03:47 +0200

    Rename MQTT server variables for consistency

--- latte-lab/config.h
+++ latte-lab/config.h
@@ -4,7 +4,7 @@
 
   const char* ssid = "YOUR_WIFI_SSID";
   const char* password = "YOUR_WIFI_PASSWORD";
-  const char* mqtt_server1 = "YOUR_MQTT_SERVER_IP.local";
-  const char* mqtt_server2 = "YOUR_MQTT_SERVER_IP.local";
+  const char* mqtt_server_1 = "YOUR_MQTT_SERVER_IP.local";
+  const char* mqtt_server_2 = "YOUR_MQTT_SERVER_IP.local";
   
 #endif
